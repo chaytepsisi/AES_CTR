1dfb4a1d0c1bf15ceb978df0987034cbb09ff4de
onurkocak <onur.kocak@outlook.com.tr>
2026-01-12 00:12:22 +0300

    + Added test vectors. + Added cancellation + Added Ctrl+V with space and "-" removal + Exception handling improved

--- AES_CTR/Form1.cs
+++ AES_CTR/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
